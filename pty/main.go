3627ec34057ab02b2a7bcbe62c513a4a1be288ec
julianzz <277600378@qq.com>
2014-06-09 21:50:00 +0800

    add makefile ,copy from consul

--- pty/main.go
+++ pty/main.go
@@ -19,18 +19,37 @@ func main() {
     log.Println( "fetch size:", cols, rows )
     if err != nil {
         panic(err)
-    }   
+    }
 
-    go func() {
+    /*go func() {
         f.Write([]byte("ls \n"))
         f.Write([]byte("date\n"))
         f.Write([]byte("time\n"))
-        f.Write([]byte{4}) // EOT 
-    }() 
-    io.Copy(os.Stdout, f)
+        f.Write([]byte{4}) // EOT
+    }()*/
+    //go io.Copy(os.Stdout, f)
 
-    c = exec.Command("date ")
+    c = exec.Command("bash")
     f2, err := pty.Start(c)
     f2.Write( []byte("ls -l \n"))
-    io.Copy( os.Stdout, f2 );
+    
+    const READSIZE = 1024
+    /*go func(){
+        for {
+            bytes     := make([]byte, READSIZE)
+            read, err := f2.Read(bytes)
+            if (err != nil) {
+                os.Stdout.Write( []byte("finish ----- ") )
+                return
+            }
+            os.Stdout.Write(bytes[:read])
+        }
+    }()
+    */
+    go func() {
+        log.Println( io.Copy( os.Stdout, f2 ) ) 
+
+    }()
+
+    io.Copy(f2, os.Stdin)
 }
